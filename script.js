b17ecceedc1ed7ce95663eef5e918cdd80585649
alnasimul <alnasimul@gmail.com>
2023-03-05 15:25:11 +0600

    playing with dom

--- script.js
+++ script.js
@@ -2,14 +2,28 @@
 
 // headerElement.style.color = 'maroon'
 
-let itemUl = document.getElementById("items")
+// let itemUl = document.getElementById("items")
 
-let items = itemUl.getElementsByClassName("item");
+// let items = itemUl.getElementsByClassName("item");
 
 // for( let element of items){
 //     element.style.color = 'red'
 // }
 
-for( let i = 0; i < items.length; i++){
-    items[i].style.color = "magenta"
-}
+// for( let i = 0; i < items.length; i++){
+//     items[i].style.color = "magenta"
+// }
+
+// let items = document.getElementsByTagName('li')
+
+// console.log(items)
+
+let lastItem = document.querySelector('.item:nth-child(1)');
+
+// for (let element of lastItems){
+//     element.style.color = 'red';
+// }
+
+lastItem.style.color = 'red'
+
+console.log(lastItem)
