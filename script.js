e7f59d5a1e0fcbcd50c59673934ed3c0a2e6216d
alnasimul <alnasimul@gmail.com>
2023-03-05 14:15:08 +0600

    again dom

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-let headerElement = document.getElementById("header")
+// let headerElement = document.getElementById("header")
 
-headerElement.style.color = 'maroon'
+// headerElement.style.color = 'maroon'
+
+let itemUl = document.getElementById("items")
+
+let items = itemUl.getElementsByClassName("item");
+
+// for( let element of items){
+//     element.style.color = 'red'
+// }
+
+for( let i = 0; i < items.length; i++){
+    items[i].style.color = "magenta"
+}
